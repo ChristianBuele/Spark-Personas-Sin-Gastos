606abf0b9212be5692edb798e930cda331267550
christian.buele <christian.buele@ucuenca.edu.ec>
2025-07-06 23:09:30 -0500

    asd

--- CategoriaDeVideosMasVista.py
+++ CategoriaDeVideosMasVista.py
@@ -5,12 +5,11 @@ import sys
 HDFS_BASE_PATH = "hdfs://localhost:9000/ejercicio2/"
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Uso: CategoriaDeVideosMasVista <input_path> <output_path>", file=sys.stderr)
+    if len(sys.argv) != 3: #valido si envia los argumentos necesarios
         sys.exit(-1)
 
-    input_path = sys.argv[1]
-    output_path = sys.argv[2]
+    input_path = sys.argv[1] # Ruta del archivo de entrada
+    output_path = sys.argv[2] # Ruta del archivo de salida
 
     spark = SparkSession.builder.appName("CategoriaDeVideosMasVista").getOrCreate()
 
@@ -20,7 +19,7 @@ if __name__ == "__main__":
     # Renombrar columnas necesarias
     df = df.withColumnRenamed("_c3", "categoria").withColumnRenamed("_c5", "vistas")
 
-    # Asegurar que 'vistas' es entero
+    # Cast a tipo entero la columna de vistas
     df = df.withColumn("vistas", col("vistas").cast("int"))
 
     # Agrupar por categoría y sumar vistas
@@ -29,7 +28,7 @@ if __name__ == "__main__":
     # Obtener la categoría más vista
     resultado = categoria_vistas.orderBy(col("total_vistas").desc()).limit(1)
 
-    # Escribir el resultado en formato CSV sin coalesce (modo distribuido)
+    # Escribir el resultado en formato CSV 
     resultado.write.option("header", True).csv(HDFS_BASE_PATH+output_path+"/")
 
     spark.stop()
