aaf18d2d4794dc1c7db88f00c97f4085dc81e403
christian.buele <christian.buele@ucuenca.edu.ec>
2025-07-06 23:36:19 -0500

    asd

--- personaYMetodosDePago.py
+++ personaYMetodosDePago.py
@@ -4,36 +4,35 @@ from pyspark.sql.functions import lower, trim
 
 hdfs_path = "hdfs://localhost:9000/ejercicio3"
 
-# Inicializar Spark
-spark = SparkSession.builder.appName("PersonaYMetodosDePago").getOrCreate()
+def menores_1500(df):# b) Compras con TDC y <= 1500 euros
+    menor_igual_1500 = df.filter(col("dinero_gastado") <= 1500)
+    return menor_igual_1500.groupBy("persona").agg(count("*").alias("compras_menor_igual_1500"))
 
-# Leer los datos (sin cabecera, usando el delimitador correcto)
-df = spark.read.option("delimiter", ";").option("header", "false").csv(hdfs_path + "/data/casoDePrueba3.txt")
-df = df.toDF("persona", "metodo_pago", "dinero_gastado")
-df.show()
+def mayores_1500(df):# a) Compras con TDC y > 1500 euros
+    mayor_1500 = df.filter(col("dinero_gastado") > 1500)
+    return mayor_1500.groupBy("persona").agg(count("*").alias("compras_mayor_1500"))
 
-# Convertir dinero_gastado a float
-df = df.withColumn("dinero_gastado", col("dinero_gastado").cast("float"))
+def read_input(spark):
+    df = spark.read.option("delimiter", ";").option("header", "false").csv(hdfs_path + "/data/casoDePrueba3.txt")
+    df = df.toDF("persona", "metodo_pago", "dinero_gastado")
+    df.show()
 
-# Filtrar solo compras con tarjeta de crédito
-df_tdc = df.filter(lower(trim(col("metodo_pago"))) == "tarjeta de crédito")
+    df = df.withColumn("dinero_gastado", col("dinero_gastado").cast("float"))  # Convertir dinero_gastado a float
+    return df
 
-# a) Compras con TDC y > 1500 euros
-mayor_1500 = df_tdc.filter(col("dinero_gastado") > 1500)
-res_mayor = mayor_1500.groupBy("persona").agg(count("*").alias("compras_mayor_1500"))
-print("Compras con TDC y > 1500 euros:")
-res_mayor.show()
-# Guardar resultado en HDFS
-res_mayor.write.mode("overwrite").csv(hdfs_path + "/comprasConTDCMayorDe1500")
+def main():
+    spark = SparkSession.builder.appName("PersonaYMetodosDePago").getOrCreate()
+    df = read_input(spark)
+    
+    df_tdc = df.filter(lower(trim(col("metodo_pago"))) == "tarjeta de crédito")# Filtrar solo compras con tarjeta de crédito
+    
+    res_menor = menores_1500(df_tdc)
+    res_menor.write.mode("overwrite").csv(hdfs_path + "/output/comprasConTDCMenoroIgualDe1500")
+    
+    res_mayor = mayores_1500(df_tdc)
+    res_mayor.write.mode("overwrite").csv(hdfs_path + "/comprasConTDCMayorDe1500")
+    
+    spark.stop()
 
-# b) Compras con TDC y <= 1500 euros
-menor_igual_1500 = df_tdc.filter(col("dinero_gastado") <= 1500)
-res_menor = menor_igual_1500.groupBy("persona").agg(count("*").alias("compras_menor_igual_1500"))
-print("Compras con TDC y <= 1500 euros:")
-res_menor.show()
-
-
-# Guardar resultado en HDFS
-res_menor.write.mode("overwrite").csv(hdfs_path + "/output/comprasConTDCMenoroIgualDe1500")
-
-spark.stop()
+if __name__ == "__main__":
+    main()
